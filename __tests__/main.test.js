edf34a746264c262aaf639b3f14274b473aa8ae7
Christopher W. Blake <chriswblake@github.com>
2024-12-31 19:09:00 +0000

    feat: Move template variable parsing into separate function. Modify it to accept JSON and ENV formats.

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -21,8 +21,8 @@ describe('action', () => {
     jest.clearAllMocks()
   })
 
-  // Proper Usage
-  it('Use template file', async () => {
+  // Proper Usage - JSON
+  it('Use template file - JSON', async () => {
     // Arrange - Mock responses for the inputs
     getInputMock.mockImplementation(inputName => {
       switch (inputName) {
@@ -64,7 +64,7 @@ describe('action', () => {
     expect(outputValue).not.toMatch(/extra value/)
   })
 
-  it('Use template text', async () => {
+  it('Use template text - JSON', async () => {
     // Arrange - Mock responses for the inputs
     getInputMock.mockImplementation(inputName => {
       switch (inputName) {
@@ -75,6 +75,38 @@ describe('action', () => {
             name: 'John1',
             extra_var: 'extra value'
           })
+        default:
+          return ''
+      }
+    })
+
+    // Act - Load the template and replace the variables
+    await main.run()
+    expect(runMock).toHaveReturned()
+
+    // Assert - Check output name
+    const call = setOutputMock.mock.calls[0]
+    const outputName = call[0]
+    expect(outputName).toBe('updated-text')
+
+    // Assert - Check inserted values
+    const outputValue = call[1]
+    expect(outputValue).toBe('Hello John1')
+    expect(outputValue).not.toMatch(/extra value/)
+  })
+
+  // Proper Usage - ENV format
+  it('Use template text - ENV', async () => {
+    // Arrange - Mock responses for the inputs
+    getInputMock.mockImplementation(inputName => {
+      switch (inputName) {
+        case 'template-text':
+          return 'Hello {{ name }}'
+        case 'template-vars':
+          return `
+            name=John1
+            extra_var=extra value
+          `
         default:
           return ''
       }
@@ -163,53 +195,137 @@ describe('action', () => {
       expect.stringMatching(/Missing required input/)
     )
   })
-})
 
-// Error Responses - Bad Inputs
-it('Provided non-JSON for template-vars. Set failed status.', async () => {
-  // Arrange - Mock responses for the inputs
-  getInputMock.mockImplementation(name => {
-    switch (name) {
-      case 'template-text':
-        return 'Hello {{ name }}'
-      case 'template-vars':
-        return 1234
-      default:
-        return undefined
-    }
+  // Error Responses - Bad Inputs
+  it('Provided number for template-vars. Set failed status.', async () => {
+    // Arrange - Mock responses for the inputs
+    getInputMock.mockImplementation(name => {
+      switch (name) {
+        case 'template-text':
+          return 'Hello {{ name }}'
+        case 'template-vars':
+          return 1234
+        default:
+          return undefined
+      }
+    })
+
+    // Act - Run action to cause the error
+    await main.run()
+    expect(runMock).toHaveReturned()
+
+    // Assert - Action was closed with correct error message
+    expect(setFailedMock).toHaveBeenNthCalledWith(
+      1,
+      expect.stringMatching(/Invalid input/)
+    )
   })
 
-  // Act - Run action to cause the error
-  await main.run()
-  expect(runMock).toHaveReturned()
+  it('Badly formed JSON for template-vars. Set failed status.', async () => {
+    // Arrange - Mock responses for the inputs
+    getInputMock.mockImplementation(name => {
+      switch (name) {
+        case 'template-text':
+          return 'Hello {{ name }}'
+        case 'template-vars':
+          return '{ forgot quotations on values }'
+        default:
+          return ''
+      }
+    })
 
-  // Assert - Action was closed with correct error message
-  expect(setFailedMock).toHaveBeenNthCalledWith(
-    1,
-    expect.stringMatching(/Invalid JSON input/)
-  )
-})
+    // Act - Run action to cause the error
+    await main.run()
+    expect(runMock).toHaveReturned()
 
-it('Badly formed JSON for template-vars. Set failed status.', async () => {
-  // Arrange - Mock responses for the inputs
-  getInputMock.mockImplementation(name => {
-    switch (name) {
-      case 'template-text':
-        return 'Hello {{ name }}'
-      case 'template-vars':
-        return '{ forgot quotations on values }'
-      default:
-        return ''
-    }
+    // Assert - Action was closed with correct error message
+    expect(setFailedMock).toHaveBeenNthCalledWith(
+      1,
+      expect.stringMatching(/Invalid input/)
+    )
   })
 
-  // Act - Run action to cause the error
-  await main.run()
-  expect(runMock).toHaveReturned()
+  it('Badly formed variable list for template-vars. Set failed status.', async () => {
+    // Arrange - Mock responses for the inputs
+    getInputMock.mockImplementation(name => {
+      switch (name) {
+        case 'template-text':
+          return 'Hello {{ name }}'
+        case 'template-vars':
+          return `
+            name!=John1
+          `
+        default:
+          return ''
+      }
+    })
 
-  // Assert - Action was closed with correct error message
-  expect(setFailedMock).toHaveBeenNthCalledWith(
-    1,
-    expect.stringMatching(/Invalid JSON input/)
-  )
+    // Act - Run action to cause the error
+    await main.run()
+    expect(runMock).toHaveReturned()
+
+    // Assert - Action was closed with correct error message
+    expect(setFailedMock).toHaveBeenNthCalledWith(
+      1,
+      expect.stringMatching(/Invalid input/)
+    )
+  })
+
+  // Methods
+  it('parseTemplateVars - JSON', () => {
+    // Arrange
+    const variables = JSON.stringify({
+      name: 'John1',
+      person: {
+        name: 'John2',
+        unused_value: 'unused'
+      },
+      multiline_paragraph: `
+      Line 1
+      Line 2
+      `,
+      extra_var: 'extra value'
+    })
+    // Act
+    const result = main.parseTemplateVars(variables)
+
+    // Assert
+    expect(result.name).toEqual('John1')
+    expect(result['person']['name']).toEqual('John2')
+    expect(result['person']['unused_value']).toEqual('unused')
+    expect(result.multiline_paragraph).toMatch(/Line 1\s*Line 2/)
+  })
+
+  it('parseTemplateVars - ENV', () => {
+    // Arrange
+    const variables = `
+      name=John1
+      person.name=John2
+      person.unused_value=unused
+      multiline_paragraph="
+      Line 1
+      Line 2
+      "
+    `
+    // Act
+    const result = main.parseTemplateVars(variables)
+
+    // Assert
+    expect(result.name).toEqual('John1')
+    expect(result['person.name']).toEqual('John2')
+    expect(result['person.unused_value']).toEqual('unused')
+    expect(result.multiline_paragraph).toMatch(/Line 1\s*Line 2/)
+  })
+
+  it('parseTemplateVars - invalid ENV', () => {
+    // Arrange
+    const variables = `
+      name!=John1
+    `
+    // Act
+    const parseAction = () => main.parseTemplateVars(variables)
+
+    // Assert
+    expect(parseAction).toThrow(/Invalid input/)
+  })
 })
